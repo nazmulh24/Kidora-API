65a0c685e6d968d89d86ecda9697a3139b312a26
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-09-06 23:26:23 +0600

    Users part done...

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,8 @@ router = routers.DefaultRouter()
 
 urlpatterns = [
     path("", include(router.urls)),
+    #
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
+    #
 ]
