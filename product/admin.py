c15c1b7925ac7367bcd40dad78732763bb7afbfc
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-09-08 01:18:50 +0600

    admin--> ProductStock...

--- product/admin.py
+++ product/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from product.models import Product, ProductImage, Category, Review, ReviewImage
+from product.models import (
+    Product,
+    ProductImage,
+    ProductStock,
+    Category,
+    Review,
+    ReviewImage,
+)
 
 admin.site.register(Product)
 admin.site.register(ProductImage)
+admin.site.register(ProductStock)
 admin.site.register(Category)
 admin.site.register(Review)
 admin.site.register(ReviewImage)
